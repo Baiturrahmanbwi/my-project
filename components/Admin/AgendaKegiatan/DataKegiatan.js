ec73a70affa6fc9cc5c39372ac0134bbd9e14f26
unknown <masjidbaiturrahmanbwi@gmail.com>
2023-05-30 23:32:26 +0700

    done1

--- components/Admin/AgendaKegiatan/DataKegiatan.js
+++ components/Admin/AgendaKegiatan/DataKegiatan.js
@@ -36,27 +36,27 @@ export default function TabelKegiatan() {
         <>
             <div className="container-xl mt-4">
                 <div className="row row-cols-2 row-cols-lg-3 g-4" style={{ marginLeft: "2rem" }}>
-                    {data.map((kegiatan,i => (
+                    {data.map((kegiatan, i) => (
                         <div className="col" >
-                            <div key={i} className="card" style={{ width: '17rem', height: '22.2rem' }}>
-                                <FotoKegiatan foto={kegiatan.fotokegiatan} />
-                                <div className="card-body">
-                                    <h5 className="card-title"><b>{kegiatan.title}</b></h5>
-                                    <p className="card-text text-justify" style={{ height: "6.5rem" }}>{kegiatan.deskripsi}
-                                        <br />
-                                        <br />
-                                        <p>Rutinan yang akan datang dilakukan pada tanggal {new Date(kegiatan.tanggal).toLocaleDateString('id-ID')}</p>
-                                    </p>
-                                    <a onClick={(event) => {
-                                        event.preventDefault();
-                                        handleDelete(kegiatan.id, kegiatan.title);
-                                    }} >
-                                        <a className="btn btn-danger text-white" style={{ fontSize: "12px", cursor: "pointer" }}><i className="bi bi-trash" style={{ fontSize: "12px", cursor: "pointer" }} data-toggle="tooltip" title="Delete"></i> Delete</a>
-                                    </a>
-                                </div>
+                        <div key={i} className="card" style={{ width: '17rem', height: '22.2rem' }}>
+                            <FotoKegiatan foto={kegiatan.fotokegiatan} />
+                            <div className="card-body">
+                                <h5 className="card-title"><b>{kegiatan.title}</b></h5>
+                                <p className="card-text text-justify" style={{ height: "6.5rem" }}>{kegiatan.deskripsi}
+                                    <br />
+                                    <br />
+                                    <p>Rutinan yang akan datang dilakukan pada tanggal {new Date(kegiatan.tanggal).toLocaleDateString('id-ID')}</p>
+                                </p>
+                                <a onClick={(event) => {
+                                    event.preventDefault();
+                                    handleDelete(kegiatan.id, kegiatan.title);
+                                }} >
+                                    <a className="btn btn-danger text-white" style={{ fontSize: "12px", cursor: "pointer" }}><i className="bi bi-trash" style={{ fontSize: "12px", cursor: "pointer" }} data-toggle="tooltip" title="Delete"></i> Delete</a>
+                                </a>
                             </div>
                         </div>
-                    )))}
+                    </div>
+                    ))}
                 </div>
             </div>
         </>
